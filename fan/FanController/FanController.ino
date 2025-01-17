0423896900f29655d246cc85032bfd134c9a23b0
Jonathan Wahrenberg <jonathan.wahrenberg@hitachigymnasiet.se>
2025-01-17 14:18:24 +0100

    Updated FanController to be functional, PID needs to be tuned with real values

--- fan/FanController/FanController.ino
+++ fan/FanController/FanController.ino
@@ -1,14 +1,18 @@
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
 #include <PIDController.h>
 
 // Wifi
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "Hitachigymnasiet_2.4"
+#define WIFI_PASSWORD "mittwifiarsabra"
 
 // Firebase RTDB
 #define DATABASE_URL "https://wexteras-9-default-rtdb.europe-west1.firebasedatabase.app/"
-#define API_KEY ""
+#define API_KEY "AIzaSyDK2H0kGevKzy-Y-5rfN8InhAOQAQVO6q8"
+#define timerDelay 200
+
+unsigned long sendDataPrevMillis = 0;
 
 // Firebase objects
 FirebaseData fbdo;
@@ -18,8 +22,10 @@ FirebaseConfig config;
 const byte MotorSpeed = 5;
 const byte MotorDir = 0;
 
-int oldPercent = 0;
+double percent = 0;
+double oldPercent = 0;
 int speed = 0;
+
 
 bool manual = false;
 
@@ -28,6 +34,38 @@ float desiredTemperature = 15.0;
 
 // PID
 PIDController pid;
+
+// Get data function
+template<typename T>
+bool getData(const char* path, T& variable) {
+  bool success = false;
+
+  if constexpr (std::is_same<T, bool>::value) {
+    success = Firebase.RTDB.getBool(&fbdo, path, &variable);
+  } else if constexpr (std::is_same<T, int>::value) {
+    success = Firebase.RTDB.getInt(&fbdo, path, &variable);
+  } else if constexpr (std::is_same<T, float>::value) {
+    success = Firebase.RTDB.getFloat(&fbdo, path, &variable);
+  } else {
+    Serial.print("Unsupported data type for path: ");
+    Serial.println(path);
+    return false;
+  }
+
+  if (success) {
+    Serial.print("Successfully fetched: ");
+    Serial.print(path);
+    Serial.print(" - Value: ");
+    Serial.println(variable);
+  } else {
+    Serial.print("Failed to fetch: ");
+    Serial.print(path);
+    Serial.print(" - Error: ");
+    Serial.println(fbdo.errorReason().c_str());
+  }
+
+  return success;
+}
 
 // Convert percentage to analog output
 int percentToSpeed(int percent) {
@@ -51,6 +89,7 @@ void setup() {
   // Start Firebase connection
   config.database_url = DATABASE_URL;
   config.api_key = API_KEY;
+  config.token_status_callback = tokenStatusCallback;
 
   Firebase.signUp(&config, &auth, "", "");
   Firebase.begin(&config, &auth);
@@ -61,41 +100,47 @@ void setup() {
   digitalWrite(MotorDir, HIGH);
 
   pid.begin();
-  pid.setpoint(20);
-  pid.tune(10, 10, 10);
+  pid.setpoint(0);
+  pid.tune(10.0, 1.0, 1.0);
   pid.limit(0, 100);
 }
 
 void loop() {
-  if (!Firebase.ready()) {
+  if (!(millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)) {
+    sendDataPrevMillis = millis();
     return;
   }
 
-  Firebase.RTDB.getFloat(&fbdo, "SensorValues/Temperature/Value", &temperature);
-  Firebase.RTDB.getFloat(&fbdo, "SensorValues/Temperature/Desired", &desiredTemperature);
-  Firebase.RTDB.getBool(&fbdo, "Overrides/Fan/Manual", &manual);
+  if (Firebase.ready()) {
 
-  int percent = 0;
-  if (manual) {
-    Serial.println("Manual mode");
-    Firebase.RTDB.getInt(&fbdo, "Overrides/Fan/Speed", &percent);
+    // Get all values
+    getData("SensorValues/Temperature/Value", temperature);
+    getData("SensorValues/Temperature/Desired", desiredTemperature);
+    if (!getData("Overrides/Fan/Manual", manual)) {
+      manual = false;
+    }
 
-  } else if (temperature < desiredTemperature) {
-    pid.setpoint(desiredTemperature);
-    percent = pid.compute(temperature);
+    Serial.println();
+    Serial.println(" - - - - ");
+
+    if (manual) {
+      Serial.println("Manual mode");
+      Firebase.RTDB.getDouble(&fbdo, "Overrides/Fan/Speed", &percent);
+
+    } else {
+      percent = pid.compute(desiredTemperature - temperature);
+      Serial.print("PID: ");
+    }
+
     Serial.println(percent);
 
-  } else {
-    Serial.println("ZERO");
-    analogWrite(MotorSpeed, 0);
-  }
+    speed = percentToSpeed(percent);
+    analogWrite(MotorSpeed, speed);
 
-  speed = percentToSpeed(percent);
-  analogWrite(MotorSpeed, speed);
-
-  // If percent has been updated from the previous value, push update to Firebase
-  if (percent != oldPercent) {
-    Firebase.RTDB.setInt(&fbdo, "MotorValues/FanSpeed", percent);
-    oldPercent = percent;
+    // If percent has been updated from the previous value, push update to Firebase
+    if (percent != oldPercent) {
+      Firebase.RTDB.setDouble(&fbdo, "MotorValues/FanSpeed", percent);
+      oldPercent = percent;
+    }
   }
 }
