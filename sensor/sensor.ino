17c9a3a3954df7a123954845fc8b5a92684ebde1
Jonathan Wahrenberg <jonathan.wahrenberg@hitachigymnasiet.se>
2025-01-21 13:36:50 +0100

    Sensor fixed :)

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,12 +1,12 @@
 // Library includes
+#include <AM2320.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <AM2320.h>
-#include <Wire.h>
 #include <TimeLib.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
 
 // Token generation process info
 #include "addons/TokenHelper.h"
@@ -14,20 +14,24 @@
 #include "addons/RTDBHelper.h"
 
 // Defines
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "Hitachigymnasiet_2.4"
+#define WIFI_PASSWORD "mittwifiarsabra"
 
-#define NTP_OFFSET 3600         // In seconds
-#define NTP_INTERVAL 60 * 1000  // In miliseconds
+#define NTP_OFFSET 3600        // In seconds
+#define NTP_INTERVAL 60 * 1000 // In miliseconds
 #define NTP_ADDRESS "europe.pool.ntp.org"
 
 // Firebase access
-#define URL ""
-#define API_KEY ""
-#define TIME_LIMIT 10000
-#define LT_TIME_LIMIT 300000
+#define URL "https://wexteras-9-default-rtdb.europe-west1.firebasedatabase.app/"
+#define API_KEY "AIzaSyDK2H0kGevKzy-Y-5rfN8InhAOQAQVO6q8"
+#define TIME_LIMIT 15000
+#define LT_TIME_LIMIT 15 * 60 * 1000
 
-//Define Firebase Data object
+String tempPath = "/Temperature";
+String humPath = "/Humidity";
+String timePath = "/Timestamp";
+
+// Define Firebase Data object
 FirebaseData fbdo;
 
 FirebaseAuth auth;
@@ -40,17 +44,11 @@ FirebaseJson json_lt;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 
-// Paths
-String tempPath = "/temperature";
-String humPath = "/humidity";
-String timePath = "/timestamp";
-
 // Sensor
 AM2320 sensor;
 
 unsigned long sendDataPrevMillis = 0;
 unsigned long sendDataLTPrevMillis = 0;
-bool signupOK = false;
 
 void setup() {
   Serial.begin(115200);
@@ -62,25 +60,17 @@ void setup() {
     Serial.print(".");
     delay(300);
   }
-  Serial.println();
-  Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
 
   // Init sensor
   Wire.begin(14, 12);
 
-
   // Assign API key and URL
   config.api_key = API_KEY;
   config.database_url = URL;
 
-  if (Firebase.signUp(&config, &auth, "", "")) {
-    Serial.println("ok");
-    signupOK = true;
-  } else {
-    Serial.println(config.signer.signupError.message.c_str());
-  }
+  Firebase.signUp(&config, &auth, "", "");
 
   // Callback function
   config.token_status_callback = tokenStatusCallback;
@@ -96,7 +86,7 @@ void loop() {
   timeClient.update();
   unsigned long epochTime = timeClient.getEpochTime();
 
-  setTime(epochTime);  // Set the time using the adjusted timestamp
+  setTime(epochTime); // Set the time using the adjusted timestamp
 
   // Extract and format the date
   String str_year = String(year(epochTime));
@@ -106,35 +96,45 @@ void loop() {
   String date = str_year + "-" + str_month + "-" + str_day;
 
   // Precheck before writing to server
-  unsigned long isReady = (millis() - sendDataPrevMillis > TIME_LIMIT || sendDataPrevMillis == 0);  // Has been more than 10s or is first send
-  if (signupOK && Firebase.ready() && isReady) {
+  unsigned long isReady =
+      (millis() - sendDataPrevMillis > TIME_LIMIT ||
+       sendDataPrevMillis == 0); // Has been more than 15s or is first send
+  if (Firebase.ready() && isReady) {
     sendDataPrevMillis = millis();
 
-    if (sensor.measure()) {  // Sensor error handling
+    if (sensor.measure()) { // Sensor error handling
       // Write JSON data
-      json.set(tempPath.c_str(), String(sensor.getTemperature()));
-      json.set(humPath.c_str(), String(sensor.getHumidity()));
+      json.set(String(tempPath.c_str()), sensor.getTemperature());
+      json.set(String(humPath.c_str()), sensor.getHumidity());
 
-      Firebase.RTDB.setJSON(&fbdo, "latest/", &json);
+      Firebase.RTDB.setJSON(&fbdo, "SensorValues/", &json);
 
-      if (millis() - sendDataLTPrevMillis > LT_TIME_LIMIT || sendDataLTPrevMillis == 0) {  // Update every 5 min
+      if (millis() - sendDataLTPrevMillis > LT_TIME_LIMIT ||
+          sendDataLTPrevMillis == 0) { // Update every 5 min
         sendDataLTPrevMillis = millis();
-        json_lt.set(tempPath.c_str(), String(sensor.getTemperature()));
-        json_lt.set(humPath.c_str(), String(sensor.getHumidity()));
+        json_lt.set(tempPath.c_str(), sensor.getTemperature());
+        json_lt.set(humPath.c_str(), sensor.getHumidity());
         json_lt.set(timePath.c_str(), epochTime);
 
-        if (Firebase.RTDB.setJSON(&fbdo, date + "/" + String(epochTime), &json_lt)) {
+        if (Firebase.RTDB.setJSON(&fbdo,
+                                  "ltStorage/" + date + "/" + String(epochTime),
+                                  &json_lt)) {
           Serial.println("Data sent successfully to Firebase.");
         } else {
-          Serial.printf("Failed to send data: %s\n", fbdo.errorReason().c_str());
+          Serial.printf("Failed to send data: %s\n",
+                        fbdo.errorReason().c_str());
         }
       }
 
-    } else {  // error has occured
+    } else { // error has occured
       int errorCode = sensor.getErrorCode();
       switch (errorCode) {
-        case 1: Serial.println("ERR: Sensor is offline"); break;
-        case 2: Serial.println("ERR: CRC validation failed."); break;
+      case 1:
+        Serial.println("ERR: Sensor is offline");
+        break;
+      case 2:
+        Serial.println("ERR: CRC validation failed.");
+        break;
       }
     }
   }
